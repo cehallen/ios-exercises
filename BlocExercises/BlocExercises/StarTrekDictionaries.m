24224fa60e6b160a534183299a1ed812ca683058
Chris Allen <cehallen@gmail.com>
2015-09-06 08:18:49 -0400

    Dictionary tests pass

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -12,17 +12,30 @@
 
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @"";
+    NSString *favDrink = characterDictionary[@"favorite drink"];
+    return favDrink;
 }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
     /* WORK HERE */
-    return @[];
+    /* 
+     initialize empty mutable array
+     iterate thru arrays to get drinks
+     add strings to mutable array
+    */
+    NSMutableArray *mutableDrinksArray = [[NSMutableArray alloc] init];
+    for (NSInteger i = 0; i < charactersArray.count ; i++) {
+        NSString *favDrinks = charactersArray[i][@"favorite drink"];
+        [mutableDrinksArray addObject:favDrinks];
+    }
+    return mutableDrinksArray;
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @{};
+    NSMutableDictionary *mutableCharacterDictionary = [characterDictionary mutableCopy];
+    [mutableCharacterDictionary setObject:@"There are no compacts between lions and men, and wolves and lambs have no concord." forKey:@"quote"];
+    return mutableCharacterDictionary;
 }
 
 @end
