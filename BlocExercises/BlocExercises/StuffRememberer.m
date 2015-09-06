0a9511c7409c825dd79d12cb346dc94e6251c726
Chris Allen <cehallen@gmail.com>
2015-09-06 17:02:09 -0400

    Make tests pass

--- BlocExercises/BlocExercises/StuffRememberer.m
+++ BlocExercises/BlocExercises/StuffRememberer.m
@@ -12,29 +12,34 @@
 
 - (void) rememberThisArrayForLater:(NSMutableArray *)arrayToRemember {
     /* WORK HERE */
+    // this should save the EXACT same array, ie same place in memory, too.
+    self.arraySavedForLater = arrayToRemember;
 }
 
 - (void) copyThisArrayForLater:(NSMutableArray *)arrayToCopy {
     /* WORK HERE */
+    // make a copy ((??) not sure if changing from propery from 'strong' to 'copy' is relevant here, but this works without it.  'copy' property assignment wouldn't matter in this case anyway, because the test occurs within the same instance...)
+    self.arrayCopiedForLater = [arrayToCopy copy];
 }
 
 - (void) rememberThisFloatForLater:(CGFloat)floatToRemember {
     /* WORK HERE */
+    self.savedCgfloat = floatToRemember;
 }
 
 - (NSMutableArray *) arrayYouShouldRemember {
     /* WORK HERE */
-    return [@[] mutableCopy];
+    return self.arraySavedForLater;
 }
 
 - (NSMutableArray *) arrayYouShouldCopy {
     /* WORK HERE */
-    return [@[] mutableCopy];
+    return self.arrayCopiedForLater;
 }
 
 - (CGFloat) floatYouShouldRemember {
     /* WORK HERE */
-    return 0.0f;
+    return self.savedCgfloat;
 }
 
 @end
