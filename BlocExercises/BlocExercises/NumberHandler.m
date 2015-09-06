f14493900e88a3a7640f5453154c1d1a214ca960
Chris Allen <cehallen@gmail.com>
2015-09-06 12:24:31 -0400

    Make tests pass

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,34 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    NSInteger num = [number integerValue] * 2;
+    return [NSNumber numberWithInt:num];
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    /*
+     intantiate empty mutable answer array
+     check for smaller NSInteger.
+     loop from smaller to bigger, 
+     converting to NSNumber,
+     and adding to mutable array on each iteration.
+     */
+    NSMutableArray *mutableNumbersArray = [[NSMutableArray alloc] init];
+    NSInteger smallerInt = number < otherNumber ? number : otherNumber;
+    NSInteger largerInt = number >= otherNumber ? number : otherNumber;
+    for (NSInteger i = smallerInt; i <= largerInt; i++) {
+        [mutableNumbersArray addObject:[NSNumber numberWithInt:i]];
+    }
+    return mutableNumbersArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    NSMutableArray *mutableArrayOfNumbers = [arrayOfNumbers mutableCopy];
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES];
+    [mutableArrayOfNumbers sortUsingDescriptors:@[sortDescriptor]];
+    return [mutableArrayOfNumbers[0] integerValue];;
 }
 
 @end
