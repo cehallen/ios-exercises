51592dde85b3975b02640c4b853aa3485692355a
Chris Allen <cehallen@gmail.com>
2015-09-09 08:04:02 -0400

    Finish tests pass

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -8,7 +8,8 @@ Strings
 
 func favoriteCheeseStringWithCheese(cheese: String) -> String {
     // WORK HERE
-    return cheese
+    var favCheeseSentence = "My favorite cheese is \(cheese)."
+    return favCheeseSentence
 }
 
 let fullSentence = favoriteCheeseStringWithCheese("cheddar")
@@ -20,13 +21,15 @@ Arrays & Dictionaries
 
 */
 
-let numberArray = [1, 2, 3, 4]
+var numberArray = [1, 2, 3, 4]
 // Add 5 to this array
 // WORK HERE
+numberArray.append(5)
 
-let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
+var numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
 // Add 5 : "five" to this dictionary
 // WORK HERE
+numberDictionary[5] = "five"
 
 /*
 
@@ -36,9 +39,15 @@ Loops
 
 // Use a closed range loop to print 1 - 10, inclusively
 // WORK HERE
+for i in 1...10 {
+    println(i)
+}
 
 // Use a half-closed range loop to print 1 - 10, inclusively
 // WORK HERE
+for i in 1..<11 {
+    println(i)
+}
 
 let worf = [
     "name": "Worf",
@@ -55,10 +64,17 @@ let picard = [
 
 let characters = [worf, picard]
 
-func favoriteDrinksArrayForCharacters(characters:[[String : String]]) -> [String] {
+func favoriteDrinksArrayForCharacters(characters:[[String : String]]) -> Array <String> {
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
     // WORK HERE
-    return []
+    /* make empty array, loop through characters array and in each pull/unwrap fav drink string and append to my array */
+    var favDrinks = [String]()
+    for char in characters {
+        if let favStr = char["favorite drink"] {
+            favDrinks.append(favStr)
+        }
+    }
+    return favDrinks
 }
 
 let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
@@ -75,7 +91,12 @@ func emailFromUserDict(userDict : [String : String]) -> String {
     // Return the user's email address from userDict, or return "" if they don't have one
     
     // WORK HERE
-    return "user@example.com"
+//    var email = ""
+    if let email = userDict["email"] {
+        return email
+    } else {
+        return ""
+    }
 }
 
 
@@ -100,6 +121,12 @@ Functions
 let strings = ["milk", "eggs", "bread", "challah"]
 
 // WORK HERE - make your function and pass `strings` in
+func joiner(stringArr: [String]) -> String {
+    var output = ";".join(stringArr)
+    return output
+}
+
+let myOutput = joiner(strings)
 
 let expectedOutput = "milk;eggs;bread;challah"
 
@@ -113,3 +140,23 @@ let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All
 
 // Use a closure to sort this array alphabetically
 // WORK HERE
+
+// QUESTION: not sure how the '<' sorts alphabetically.  I thought it only compares two by two, and if comparison is 'true' returns first val first.  So how does it sort the whole array?  Do I need to write a quicksort or something and if so why is this working..
+
+// QUESTION: why does "a" < "b" evaluate to true but "b" > "a" error out?  What if I want to sort alphabetically, backwards?
+
+let sortedCereals = sorted(cerealArray, {(cereal1: String, cereal2: String) -> Bool in
+        return cereal1 < cereal2
+    })
+
+//// one way a little shorter
+//let sortedCereals = sorted(cerealArray, {c1, c2 in c1 < c2})
+//println(sortedCereals)
+
+//// or use $0, $1 shortcuts for items
+//let sortedCereals = sorted(cerealArray, {$0 < $1})
+//println(sortedCereals)
+
+//// shorter still
+//let sortedCereals = sorted(cerealArray, <)
+//println(sortedCereals)
